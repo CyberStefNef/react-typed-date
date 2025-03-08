33fd41dc86d74280d0de0acc9089eb23bdeac83a
CyberStefNef <stef.mue02@gmail.com>
2025-03-08 02:16:09 +0100

    fix docusaurus baseUrl

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: "https://cyberstefnef.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/react-typed-date/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
