d6081121b478183c02e3e63a8532300952463f45
CyberStefNef <stef.mue02@gmail.com>
2025-09-10 14:20:36 +0200

    fix: added missing dependency for layout update

--- src/useTypedDate.ts
+++ src/useTypedDate.ts
@@ -302,7 +302,7 @@ export function useTypedDate({
 
     return hasTime ? `${dateString} ${timeString}` : dateString;
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [segmentOrder, dateSeparator, timeSeparator, hasTime]);
+  }, [segmentOrder, dateSeparator, timeSeparator, hasTime, state]);
 
   useEffect(() => {
     if (inputRef.current) {
